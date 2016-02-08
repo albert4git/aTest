0ec20841cf670032c655a74636a21d366ea08348
albert <berman.albert@gmail.com>
2016-02-08 23:25:12 +0100

    bash king2

--- dotFiles/king.sh
+++ dotFiles/king.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+# ln -sf /full/path/to/parent/child/child ../child1
+# ln -sf /full/path/to/parent/child/child ../child2
+# To make a link to a file in another user's home directory:
+# ln ~sandra/prog.c .
+
+
+
 mv ~/.bashrc ~/1bashrc
-ln -s ./bashrc_color ~/.bashrc 
+ln -s ~/git/aTest/dotFiles/bashrc_color ~/.bashrc 
 
 mv ~/.bashrc_help ~/1bashrc_help 
-ln -s ./bashrc_help_color ~/.bashrc_help 
+ln -s ~/git/aTest/dotFiles/bashrc_help_color ~/.bashrc_help 
+
