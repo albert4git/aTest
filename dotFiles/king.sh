51d896b506fc582c540a8d18944bb863cf5f2389
Albert <berman.albert@gmail.com>
2017-10-26 07:19:09 +0200

    dot king

--- dotFiles/king.sh
+++ dotFiles/king.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#/bin/bash
 # ln -sf /full/path/to/parent/child/child ../child1
 # ln -sf /full/path/to/parent/child/child ../child2
 # To make a link to a file in another user's home directory:
@@ -6,9 +6,15 @@
 
 
 
-mv ~/.bashrc ~/1bashrc
-ln -s ~/git/aTest/dotFiles/bashrc_color ~/.bashrc 
+mv ~/.bashrc ~/"1bashrc-$(date +"%m-%d-%y-%r")"
+ln -s ~/atest/dotFiles/bashrc_color ~/.bashrc 
 
-mv ~/.bashrc_help ~/1bashrc_help 
-ln -s ~/git/aTest/dotFiles/bashrc_help_color ~/.bashrc_help 
+mv ~/.bashrc_help ~/"1bashrc_help-$(date +"%m-%d-%y-%r")"
+ln -s ~/atest/dotFiles/bashrc_help_color ~/.bashrc_help 
 
+mv ~/.emacs.d/init.el ~/.emacs.d/"1emacs-$(date +"%m-%d-%y-%r")"
+ln -s ~/atest/dotFiles/initEm2.el ~/.emacs.d/init.el
+
+
+mv ~/.vimrc ~/"1vimrc-$(date +"%m-%d-%y-%r")"
+ln -s ~/atest/dotFiles/vimrc2008 ~/.vimrc 
