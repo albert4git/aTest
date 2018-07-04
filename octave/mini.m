9b9bb3915602bf7dffc5c7167884f8b7f7cbcd29
Albert <berman.albert@gmail.com>
2018-07-04 04:39:30 -0400

    juli 4 aenderung 1

--- octave/mini.m
+++ octave/mini.m
@@ -1,9 +1,8 @@
 
 ###########################################################################
-# example octave script 1
+# example octave script Nr0
 ###########################################################################
 #!/usr/bin/octave -qf
-
  printf ("Hello, world!\n");
 
 #!/usr/bin/octave -qf
@@ -11,10 +10,12 @@ x = linspace(0, 2*pi, 100);
 y = sin(x);
 plot(x, y);
 figure;
-printf ("Hello, world!\n");
+
+print -dpng -color "-S500,500" sin0.png
+open ("sin0.png");
 
 ###########################################################################
-# example octave script 9
+# example octave script Nr9
 ###########################################################################
 #!/usr/bin/octave -qf
 x = -10:0.1:10;
@@ -25,6 +26,6 @@ ylabel ("sin (x)");
 text (pi, 0.7, "arbitrary text");
 legend ("sin (x)");
 
-print -dpng -color "-S500,500" sin0.png
-open ("sin0.png");
+print -dpng -color "-S500,500" sin9.png
+open ("sin9.png");
 ###########################################################################
