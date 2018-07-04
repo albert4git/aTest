61103b9285ee927f343c8a85e9335767b04393be
Albert <berman.albert@gmail.com>
2018-07-03 21:43:49 -0400

    Mini Bsp.

---
+++ octave/mini.m
@@ -0,0 +1,13 @@
+
+###########################################################################
+#!/usr/bin/octave -qf
+ # a sample Octave program
+ printf ("Hello, world!\n");
+
+#!/usr/bin/octave -qf
+x = linspace(0, 2*pi, 100);
+y = sin(x);
+plot(x, y);
+figure;
+printf ("Hello, world!\n");
+
