249026f3887eebe56d670d9e90bb126b42137f25
Albert <berman.albert@gmail.com>
2019-02-02 20:19:25 +0100

    pyNachShubA

---
+++ octave/testJan2019.m
@@ -0,0 +1,34 @@
+exp (i*pi)
+
+A = [ 1, 1, 2; 3, 5, 8; 13, 21, 34 ]
+
+B = rand (3, 2);
+
+2 * A
+ 
+B*A
+
+## Demo of how to use a number (which was calculated in an octave
+## variable) in a symbolic calculation, without getting a warning.
+
+## use octave to calculate some number:
+ a = pi/2
+
+## now do some work with the symbolic pkg
+ syms x
+ f = x * cos (x)
+ df = diff (f)
+
+## Now we want to evaluate df at a:
+
+ # subs (df, x, a)     # this gives the "rats" warning (and gives a symbolic answer)
+
+## So instead, try
+
+ dfh = function_handle (df)
+
+ dfh (a)
+ 
+ ## And you can evaluate dfh at an array of "double" values:
+
+ dfh ([1.23 12.3 pi/2])
